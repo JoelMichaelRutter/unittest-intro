436b93d9a476a8cecc78e0950924884cf94f266e
JoelMichaelRutter <joel-rutter@live.co.uk>
2021-10-25 19:57:07 +0000

    initial commit

---
+++ evens.py
@@ -0,0 +1,14 @@
+def even_number_of_evens(numbers):
+    if isinstance(numbers, list):
+        evens = sum([1 for n in numbers if n % 2 == 0])
+        return True if evens and evens % 2 == 0 else False
+        if evens:
+            return evens % 2 == 0
+        else:
+            return False
+    else:
+        raise TypeError("A list was not passed into the function")
+
+
+if __name__ == '__main__':
+    print(even_number_of_evens(5))
